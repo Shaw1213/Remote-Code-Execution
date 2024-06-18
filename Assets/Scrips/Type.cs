896472caedb6971fd0dd18d114b3e8a4040a0e10
Shawn <mdzzsb233@gmail.com>
2024-06-18 17:30:17 -0600

    typer complet

--- Assets/Scrips/Type.cs
+++ Assets/Scrips/Type.cs
@@ -1,18 +1,80 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Type : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Text wordOutput = null;
+
+    private string remaingwords = string.Empty;
+    private string currentWord = string.Empty;
+
+    private void Start()
     {
-        
+        setCurrentWord();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void setCurrentWord()
     {
-        
+        currentWord = "apple";
+        setRemaingWords(currentWord);
     }
+
+    private void setRemaingWords(string newString)
+    {
+        remaingwords = newString;
+        wordOutput.text = remaingwords;
+    }
+    private void CheckImput()
+    {
+        if(Input.anyKeyDown)
+        {
+            String keyPressed = Input.inputString;
+            
+            if(keyPressed.Length == 1)
+            {
+                EntterLetter(keyPressed);
+            }
+        }
+    }
+
+    private void EntterLetter (string typedLetter)
+    {
+        if (IsCurrentLetter(typedLetter))
+        {
+            RemoveLetter();
+
+            if(IswordComplete())
+            {
+                setCurrentWord();
+            }
+        }
+    }
+
+    private bool IsCurrentLetter(string letter)
+    {
+        return remaingwords.IndexOf(letter) == 0;
+    }
+
+    private void RemoveLetter()
+    {
+        String newString = remaingwords.Remove(0, 1);
+        setRemaingWords(newString);
+    }
+
+    private bool IswordComplete()
+    {
+        return remaingwords.Length == 0;
+
+    }
+
+    private void Update()
+    {
+        CheckImput();
+    }
+
+
 }
